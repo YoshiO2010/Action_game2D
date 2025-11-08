bd3085491f6dc4b44e635df02aeba212a6d9714c
YoshiO2010 <134188240+YoshiO2010@users.noreply.github.com>
2025-11-08 18:28:56 +0900

    2025 11 08
    スキルセレクト画面の製作、スキルUIを作った

---
+++ Assets/script/Ability_deta.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Ability_deta")]
+
+public class Ability_deta : ScriptableObject
+{
+    // Start is called before the first frame update
+    public string id;
+    public string name;
+    public Sprite icon;
+    public string class_name;
+}
---
+++ Assets/script/Skillselect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skillselect : MonoBehaviour
+{
+    [SerializeField]
+    Ability_deta[] skill_list;
+    [SerializeField]
+    Sprite[] skill_icon_list;
+    [SerializeField]
+    GameObject[] Set_skill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
